550569fb1a0a5d6e3e3c36b92a05c0727746b347
ridwanam9 <ridwanamirulmaulana8@gmail.com>
2025-06-01 11:49:46 +0700

    minimal flask app

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from tasktracker-backend!")
+# def main():
+#     print("Hello from tasktracker-backend!")
 
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
